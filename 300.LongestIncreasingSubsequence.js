e9834c42083196b1e6f63109e86078425898859e
caotrongtin99 <tin.caotrong@gmail.com>
2022-07-29 23:37:51 +0700

    300. Longest Increasing Subsequence

---
+++ 300.LongestIncreasingSubsequence.js
@@ -0,0 +1,17 @@
+/**
+ * Problem: https://leetcode.com/problems/longest-increasing-subsequence
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var lengthOfLIS = function(nums) {
+  const L = new Array(nums.length).fill(1)
+  
+  for (let i = 1; i < nums.length; i++) {
+      for (let j = 0; j < i; j++) {
+          if (nums[j] < nums[i] && L[j] + 1 > L[i]) {
+              L[i] = L[j] + 1
+          }
+      }
+  }
+  return Math.max(...L);
+};
