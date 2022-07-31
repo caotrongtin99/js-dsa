31f73dca9ee73e7d7423148eb9934021b48ea913
caotrongtin99 <tin.caotrong@gmail.com>
2022-07-31 16:06:27 +0700

    125. Valid Panlindrome

---
+++ 125.ValidPalindrome.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: https://leetcode.com/problems/valid-palindrome/
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isPalindrome = function(s) {
+  s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] !== s[s.length - i - 1]) {
+          return false
+      }
+  }
+  
+  return true
+};
