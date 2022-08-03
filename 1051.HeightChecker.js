112e6bca605cce477ebe548af6cac87ad36199a1
caotrongtin99 <tin.caotrong@gmail.com>
2022-08-03 10:18:03 +0700

    1051. Height Checker

---
+++ 1051.HeightChecker.js
@@ -0,0 +1,15 @@
+/**
+ * Problem: https://leetcode.com/problems/height-checker/
+ * @param {number[]} heights
+ * @return {number}
+ */
+ var heightChecker = function(heights) {
+  const sortedHeight = [...heights].sort((a, b) => a - b)
+  let count = 0
+
+  for (let i = 0; i < heights.length; i++) {
+      if (heights[i] !== sortedHeight[i]) count++
+  }
+  
+  return count
+};
